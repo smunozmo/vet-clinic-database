e9a49be18ac50f37835860bbc3331e64a377637c
Sebastián M <smunozmo@gmail.com>
2021-09-21 16:29:21 -0500

    Create table.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id integer NOT NULL,
+    name text,
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg numeric,
+    PRIMARY KEY (id)
 );
+
