214ea9856905af593a3cf32dab8c2e877d8b0477
Sebastián M <smunozmo@gmail.com>
2021-09-22 16:27:05 -0500

    Minor change.

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+-- Day 1
+
 SELECT * FROM animals WHERE name LIKE '%mon';
 
 SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '12-31-2019';
@@ -15,3 +17,60 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- Day 2
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > '01-01-2022';
+
+SAVEPOINT p1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO p1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, AVG(escape_attempts)
+FROM animals
+GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth BETWEEN '01-01-1990' AND '12-31-2000'
+GROUP BY species;
