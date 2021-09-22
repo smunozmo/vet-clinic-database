87b03c0a6d51882ba9d0a89343e3e52f5d1c496d
Sebastián M <smunozmo@gmail.com>
2021-09-21 19:25:05 -0500

    Update review changes.

--- data.sql
+++ data.sql
@@ -1,17 +1,17 @@
 /* Populate database with sample data. */
 
-INSERT INTO public.animals(
+INSERT INTO animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES ( 2, 'Gabumon', '11-15-2018', 2, true, 8);
+	VALUES ( 2, 'Gabumon', '11-15-2018', 2, 'true', 8);
 
-INSERT INTO public.animals(
+INSERT INTO animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES ( 1, 'Agumon', '02-03-2020', 0, true, 10.23);
+	VALUES ( 1, 'Agumon', '02-03-2020', 0, 'true', 10.23);
 
-INSERT INTO public.animals(
+INSERT INTO animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES ( 3, 'Pikachu', '01-07-2021', 1, false, 15.04);
+	VALUES ( 3, 'Pikachu', '01-07-2021', 1, 'false', 15.04);
 
-INSERT INTO public.animals(
+INSERT INTO animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES ( 4, 'Devimon', '05-12-2017', 5, true, 11);
+	VALUES ( 4, 'Devimon', '05-12-2017', 5, 'true', 11);
--- queries.sql
+++ queries.sql
@@ -1,16 +1,16 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM public.animals WHERE name LIKE '%mon';
+SELECT * FROM animals WHERE name LIKE '%mon';
 
-SELECT name FROM public.animals WHERE date_of_birth BETWEEN '01-01-2016' AND '12-31-2019';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '12-31-2019';
 
-SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
-SELECT * FROM animals WHERE neutered;
+SELECT * FROM animals WHERE neutered = true;
 
 SELECT * FROM animals WHERE name != 'Gabumon';
 
